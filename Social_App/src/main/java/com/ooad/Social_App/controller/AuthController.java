1218c8f7dd26268c20a57945c7d41f2d5c222127
chetanaaaa <chetana.p2003@gmail.com>
2024-04-11 16:13:30 +0530

    auth

--- Social_App/src/main/java/com/ooad/Social_App/controller/AuthController.java
+++ Social_App/src/main/java/com/ooad/Social_App/controller/AuthController.java
@@ -9,6 +9,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,82 +24,92 @@ import com.ooad.Social_App.model.User;
 import com.ooad.Social_App.response.AuthResponse;
 import com.ooad.Social_App.service.CustomUserDetailsServiceImplementation;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private JwtProvider jwtProvider;
-	
+
 	@Autowired
 	private CustomUserDetailsServiceImplementation customUserDetails;
-	
-	
+
 	@PostMapping("/signup")
-	public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException{
-		
-		System.out.println("user "+ user);
+	public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException {
+
+		System.out.println("user " + user);
 		String email = user.getEmail();
 		String password = user.getPassword();
 		String fullName = user.getFullName();
 		String birthDate = user.getBirthDate();
-		
+
 		User isEmailExist = userRepository.findByEmail(email);
-		
-		if(isEmailExist!=null) {
+
+		if (isEmailExist != null) {
 			throw new UserException("Email is already used with another account");
 		}
-		
+
 		User createdUser = new User();
 		createdUser.setEmail(email);
 		createdUser.setFullName(fullName);
 		createdUser.setPassword(passwordEncoder.encode(password));
 		createdUser.setBirthDate(birthDate);
-		
+
 		User savedUser = userRepository.save(createdUser);
-		
+
 		Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
+
 		String token = jwtProvider.generateToken(authentication);
-		
-		AuthResponse res = new AuthResponse(token,true);
-		
-		return new ResponseEntity<AuthResponse>(res,HttpStatus.CREATED);
-	}
-	
-	@PostMapping("/signin")
-	public ResponseEntity<AuthResponse> signin(@RequestBody User user){
-		String username = user.getEmail();
-		String password = user.getPassword();
-		
-		Authentication authentication = authenticate(username, password);
-				
-		String token = jwtProvider.generateToken(authentication);
-		
-		AuthResponse res = new AuthResponse(token,true);
-		
-		return new ResponseEntity<AuthResponse>(res,HttpStatus.ACCEPTED);
-	
+
+		AuthResponse res = new AuthResponse(token, true);
+
+		return new ResponseEntity<AuthResponse>(res, HttpStatus.CREATED);
 	}
 
-	
+	@PostMapping("/signin")
+	public ResponseEntity<AuthResponse> signin(@RequestBody User user) {
+		String username = user.getEmail();
+		String password = user.getPassword();
+
+		Authentication authentication = authenticate(username, password);
+
+		String token = jwtProvider.generateToken(authentication);
+
+		AuthResponse res = new AuthResponse(token, true);
+
+		return new ResponseEntity<AuthResponse>(res, HttpStatus.ACCEPTED);
+
+	}
+
+	@GetMapping("/signup")
+	public String showSignupForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@GetMapping("/signin")
+	public String showSigninForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signin";
+	}
+
 	private Authentication authenticate(String username, String password) {
-		UserDetails userDetails=customUserDetails.loadUserByUsername(username);
-		
-		if(userDetails == null) {
+		UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+		if (userDetails == null) {
 			throw new BadCredentialsException("invalid username");
 		}
-		if(!passwordEncoder.matches(password, userDetails.getPassword())) {
+		if (!passwordEncoder.matches(password, userDetails.getPassword())) {
 			throw new BadCredentialsException("invalid username or password");
 		}
-		return new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 	}
-	
+
 }
